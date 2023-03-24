87b50c5e104c483a982d819004736be496760f02
alechkos <alechqos@gmail.com>
2023-03-25 00:04:56 +0300

    added respective middleware

--- digitalwarehouse-web/settings.py
+++ digitalwarehouse-web/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "digitalwarehouse-web.urls"
