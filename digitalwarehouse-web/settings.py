0425bc1fc38e068abcf8ed7743a59b413f86cd37
alechkos <alechqos@gmail.com>
2023-03-25 00:07:17 +0300

    added internal ip

--- digitalwarehouse-web/settings.py
+++ digitalwarehouse-web/settings.py
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
