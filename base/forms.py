996f7368277e6a42750af971ee975ece4373393d
alechkos <alechqos@gmail.com>
2023-04-21 23:01:53 +0300

    BSPMC238-37 complited form for password changing

--- base/forms.py
+++ base/forms.py
@@ -1,13 +1,44 @@
+from tkinter import NO
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import AuthenticationForm, SetPasswordForm, PasswordResetForm
+
+User = get_user_model()
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label="שם משתמש/ת", widget=forms.TextInput())
-    password = forms.CharField(label="סיסמה", widget=forms.PasswordInput())
+    username = forms.CharField(
+        label="שם משתמש/ת",
+        widget=forms.TextInput(attrs={"autofocus": True}))
+    password = forms.CharField(
+        label="סיסמה",
+        strip=False,
+        widget=forms.PasswordInput())
 
     def __init__(self, request: None, *args, **kwargs) -> None:
         super().__init__(request, *args, **kwargs)
         self.error_messages["invalid_login"] = (
             "שם משתמש/ת ו/או סיסמה לא נכונים")
+
+    def change_password_is_required(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+        identity_num = User.objects.filter(
+            username=username).values_list("identity_num", flat=True).first()
+        if password == identity_num:
+            return True
+        return False
+
+
+class PasswordSetForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label="בחר/י סיסמה חדשה",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autofocus": True}),
+        help_text=password_validation.password_validators_help_text_html())
+    new_password2 = forms.CharField(
+        label="אמת/י את הסיסמה",
+        strip=False,
+        widget=forms.PasswordInput())
