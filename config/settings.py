f8819ffee6eb03d0a02269654fa99c598a3609f2
alechkos <alechqos@gmail.com>
2023-04-29 16:21:53 +0300

    requirements updated
    - added Pillow

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "debug_toolbar",
+    
     "base.apps.BaseConfig",
 ]
 
